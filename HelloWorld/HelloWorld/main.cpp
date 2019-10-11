21f6340d3ac3fd92bbf85dbc2634bf2e950588d0
Trevis J. Fields <tfields3@student.rccd.edu>
2019-10-11 00:30:41 -0700

    Simple Hello World
    Created a simple Hello World program and added a lot of comments that wil help define the rest of the programming level and expections for this type of coding as well as the different types of programs thart will be writen along the way that will need more than one set of eyes looking over the material and using it for thier own personal gain.

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+// Trevis J Fields
+// CPP PROGRAMMING ON A MAC USING XCODE IDE
+// PROGRAM CREATED OCT 11TH @ 00:13
+
+// A SIMPLE HELLO WORLD PROGRAM TO DISPLAY OUTPUT TO THE USER AND THEN UPLOAD TO GITHUB
+
+// standard library
+#include <iostream>
+
+// standard library
+using namespace std;
+
+int main()
+// begining of the program
+{ // opening brace of main
+		 
+		 // displays "output" to the user --> 'Hello World!' is displayed to the user while the program is running.
+		 cout << "Hello World!" << endl;
+		 
+		 return 0; // returns that the program has ended successfully and there are no problems to be had at a later date.
+} // closing brace of main
+// end of the program
+
+/*
+ --> NOTES:
+ - There are a few major things you will need in order to create and write cpp programs.
+ - one of the main things you will need si to have a standard library in order to execute different tasks and functions that would otherwise be long and tedious
+ lines of code that would take up more space and make programs a lot longer to debug.
+ - Another thing that is good for programs is to make various comments so that when you debug you can see where you werer before and after the changes as well if other programmers have to fix your code they will be able to spot differernt locations and leave comments back for you to see that there is a successful change or if there is still work to be done.
+ - Lastly (for now) you should always save and upload or back up your programs after new lines of code are added or deleted and also any and everytime you do a test build of the program, so in this way you will always have the latest and best working version of the program at your disposal when the time comes for you or another programmer to release a beta test or to create the final product of something.
+ */
+
+
+// ** There are only 7 lines of code for this entire program to run successfully, however i added in a bunch of comments so that way a new programmer looking at this as an example will see that although programming is not an easy thing to do it is not by any means a hard task to grasp and make wonserful programs for others to use.
+
+// ** later on there will be programs that will be linked to other files that reside in other repositories like JAVA, JavaScript, PHP, CSS, HTML, and other programming languages.
