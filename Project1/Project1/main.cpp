e5758e144f8c7cd98e5092e4820e091c4e97887f
Trevis J. Fields <tfields3@student.rccd.edu>
2019-10-14 18:19:56 -0700

    Project 1
    Started working on Project 1.

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Trevis Jontaé Fields on 10/14/19.
+//  Copyright © 2019 Trevis J. Fields. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		 cout << "Welcome to the Project Number 1./n";
+		 return 0;
+}
